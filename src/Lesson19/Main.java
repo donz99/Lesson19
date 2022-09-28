5947c945c9c17b85d54767f51b09fd8993e07eac
DenisK <35003466+donz99@users.noreply.github.com>
2022-09-28 17:57:15 +0300

    Update Main.java

--- src/Lesson19/Main.java
+++ src/Lesson19/Main.java
@@ -16,4 +16,59 @@ public class Main {
         }
 
     }
+    //class Main {
+  public static void main(String[] args) {
+
+    //7. Животные
+// Создать класс Animal и расширяющие его классы Dog, Cat, Horse.
+// Класс Animal содержит переменные food, location и методы makeNoise, eat, sleep. Метод makeNoise, например, может выводить на консоль "Такое-то животное спит". 
+// Dog, Cat, Horse переопределяют методы makeNoise, eat. 
+// Добавьте переменные в классы Dog, Cat, Horse, характеризующие только этих животных.
+// Создайте класс Ветеринар, в котором определите метод void treatAnimal(Animal animal). Пусть этот метод распечатывает food и location пришедшего на прием животного.
+// В методе main создайте массив типа Animal, в который запишите животных всех имеющихся у вас типов. В цикле отправляйте их на прием к ветеринару.
+    
+    System.out.println("Hello world!");
+  }
+}
+
+class Animal {
+  String name;
+  String food;
+  String location;
+
+  public Animal(){}
+
+  
+
+  public Animal()
+
+  public int makeNoise(){
+    System.out.println(this.name +" спит. ZzZz");
+    
+  }
+
+  public int eat (){
+    
+  }
+
+  public void sleep (){
+    
+  }
+  
+}
+
+class Dog extends Animal{
+  
+}
+
+class Cat extends Animal {
+  
+}
+
+class Horse extends Animal {
+  
+}
+
+
+    
 }
